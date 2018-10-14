36bdece76ba6bc0f95714712e8c8b1602450f5f0
REDMOND\shweaver <shweaver@microsoft.com>
2018-10-14 10:04:07 -0700

    Adding StageControl stubs

--- FluentControls/Controls/StageControl/StageControl.cs
+++ FluentControls/Controls/StageControl/StageControl.cs
@@ -1,14 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace Pseudo3DToolkit.Controls
 {
+    [TemplatePart(Name = CAMERACONTROL_NAME, Type = typeof(CameraControl))]
+    [TemplatePart(Name = CONTENTPRESENTER_NAME, Type = typeof(ContentPresenter))]
     public sealed partial class StageControl : ContentControl
     {
+        // Template part names
+        private const string CAMERACONTROL_NAME = "MyCamera";
+        private const string CONTENTPRESENTER_NAME = "MyContent";
+
+        // Template parts
+        private CameraControl _cameraControl;
+        private ContentPresenter _contentPresenter;
+
         public StageControl()
         {
             DefaultStyleKey = typeof(StageControl);
@@ -16,9 +22,43 @@ namespace Pseudo3DToolkit.Controls
 
         protected override void OnApplyTemplate()
         {
-
+            SetupContentPresenter();
+            SetupCameraControl();
 
             base.OnApplyTemplate();
+        }
+
+        private void SetupContentPresenter()
+        {
+            if (_contentPresenter != null)
+            {
+
+            }
+
+            _contentPresenter = (ContentPresenter)GetTemplateChild(CONTENTPRESENTER_NAME);
+
+            if (_contentPresenter != null)
+            {
+
+            }
+        }
+
+        private void SetupCameraControl()
+        {
+            // Unregister
+            if (_cameraControl != null)
+            {
+
+            }
+
+            // Get template part
+            _cameraControl = (CameraControl)GetTemplateChild(CAMERACONTROL_NAME);
+
+            // Bail if missing
+            if (_cameraControl == null) return;
+
+            // Camera setup
+
         }
     }
 }
